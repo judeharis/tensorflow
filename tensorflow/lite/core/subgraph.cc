99a6184a4cff7b897a0b1fe17f179e626f7c40a1
Jude Haris <j.haris.1@research.gla.ac.uk>
2025-01-13 17:44:09 +0000

    Updated launch and task configs

--- tensorflow/lite/core/subgraph.cc
+++ tensorflow/lite/core/subgraph.cc
@@ -1691,7 +1691,7 @@ TfLiteStatus Subgraph::InvokeImpl() {
     EnsureTensorsVectorCapacity();
     tensor_resized_since_op_invoke_ = false;
     // Jude: Added
-    // std::cout << "node_index: " << node_index << std::endl;
+    // std::cout << "node_index: " << node_index <<  " node name: " << GetTFLiteOpName(registration) << std::endl;
     if (auto s = OpInvoke(registration, &node); s != kTfLiteOk) {
       auto err = ReportOpError(&context_, node, registration, node_index,
                                "failed to invoke");
