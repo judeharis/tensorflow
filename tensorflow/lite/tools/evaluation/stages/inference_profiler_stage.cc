a42677b7d12a75e8b557c16db06083d71bcac507
Jude Haris <j.haris.1@research.gla.ac.uk>
2025-05-13 10:59:33 +0100

    Removed Inference Difference logging error per element

--- tensorflow/lite/tools/evaluation/stages/inference_profiler_stage.cc
+++ tensorflow/lite/tools/evaluation/stages/inference_profiler_stage.cc
@@ -62,17 +62,17 @@ float CalculateAverageError(T* reference, T* test, int64_t num_elements) {
     float test_value = static_cast<float>(test[i]);
     float reference_value = static_cast<float>(reference[i]);
      // Jude: Added
-    if (reference_value != test_value) {
-      LOG(INFO) << "Mismatch at index " << i
-                << ": reference = " << reference_value
-                << ", test = " << test_value
-                << " (diff =" << std::abs(test_value - reference_value) << ")"
-                << std::endl;
-    }
+    // if (reference_value != test_value) {
+    //   LOG(INFO) << "Mismatch at index " << i
+    //             << ": reference = " << reference_value
+    //             << ", test = " << test_value
+    //             << " (diff =" << std::abs(test_value - reference_value) << ")"
+    //             << std::endl;
+    // }
     error += std::abs(test_value - reference_value);
-    if ((test_value - reference_value) != 0) {
-      LOG(INFO) << "error: " << error << std::endl;
-    }
+    // if ((test_value - reference_value) != 0) {
+    //   LOG(INFO) << "error: " << error << std::endl;
+    // }
   }
   error /= num_elements;
 
